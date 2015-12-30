7bcc3b006110efc6e77d9fad79815bef0e8c9c7d
Scott Krulcik <skrulcik@andrew.cmu.edu>
2015-12-30 02:01:23 -0500

    Removed system color picker from form
    Removed because iOS (which I plan to use a lot with this) doesn't have a
    system color picker, so the user would have to mess with hex values.
    
    New version uses RGB sliders with a live-updated preview.

--- web/js/main.js
+++ web/js/main.js
@@ -1,19 +1,37 @@
-/* From SO user Plippie */
-function getParameterByName( name ) {
-    var regexS = "[\\?&]"+name+"=([^&#]*)", 
-    regex = new RegExp( regexS ),
-    results = regex.exec( window.location.search );
-    if( results == null ){
-        return "";
-    }
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
+
+/* Color Picker with Preview
+ *
+ * RGB to Hex: http://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+ */
+function rawHex(r, g, b) {
+    return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+function rgbToHex(r, g, b) {
+    return "#" + rawHex(r, g, b);
 }
 
-var color =getParameterByName('color');
-if (color) {
-    $('input[name=color]').val(color);
-} else {
-    console.log("Color not given, falling back to default\n");
-}
+function updateColor() {
+    var color = rgbToHex(
+        Number($('input[name=r]').val()),
+        Number($('input[name=g]').val()),
+        Number($('input[name=b]').val())
+    );
 
+    $('#color-preview').css('background-color', color);
+}
+updateColor();
+
+$('input[type=range]').change(function() {
+    updateColor();
+});
+
+
+function sendColor() {
+    $.post("http://newpi.local:8080/", "color=%23" + rawHex(
+        Number($('input[name=r]').val()),
+        Number($('input[name=g]').val()),
+        Number($('input[name=b]').val())
+    ));
+    return false;
+}
 
